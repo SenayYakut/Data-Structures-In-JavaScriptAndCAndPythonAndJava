f82ec095cd0fde56f946a26353b7f219e12ed2bc
Senay Yakut <senaykt@gmail.com>
2020-03-09 14:08:22 -0700

    you can store either in hash object or array using hashtables

---
+++ Algorithms/twoSum.js
@@ -0,0 +1,15 @@
+function twoSum(numArray, sum){
+    var pairs=[];
+    var hashTable=[];
+    for(var i=0; i<numArray.length; i++){
+        var currNum=numArray[i];
+        var counterPart=sum-currNum;
+        if(hashTable.indexOf(counterPart)!==-1){
+            pairs.push([currNum, counterPart]);
+        }
+        hashTable.push(currNum);
+    }
+    return pairs;
+}
+
+console.log(twoSum([1,6,4,5,3,3],7));
