8f6fb0c32c62b633ad6ec27b63ec6581a7bc1062
Senay Yakut <senaykt@gmail.com>
2020-03-17 06:54:13 -0700

    Exponential run time

--- Algorithms/Fibonacci.js
+++ Algorithms/Fibonacci.js
@@ -4,3 +4,17 @@ let fib=(num)=>{
     return num<2 ? num:fib(num-1)+fib(num-2);
 }
 console.log(fib(9));
+
+//compute the fibonacci number position
+//if we use recursion here, it gives us a very bad time complexity
+//exponontial which is very bad. O(2^n)
+function fibonacci(position){
+    if(position <3){
+      return position;
+    }
+    else{
+      return fibonacci(position-1)+fibonacci(position-2);
+    }
+  }
+  
+  console.log(fibonacci(30));
