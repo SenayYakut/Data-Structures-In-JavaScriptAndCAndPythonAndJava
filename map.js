96c40f3188232ea5cd5a13e5bb135c22fcd58049
Senay Yakut <senaykt@gmail.com>
2020-03-27 07:44:50 -0700

    map in javascript

---
+++ map.js
@@ -0,0 +1,16 @@
+//Map in JavaScript
+var map1=new Map([[1,2],[2,3],[3,4],[4,5]]);
+console.log("Map1");
+console.log(map1);
+
+var map2=new Map([["firstname","Senay"],["lastname","Yakut"],["website","github/Senaykt"]]);
+console.log("Map2");
+console.log(map2);
+
+var map3=new Map([["whole numbers",[1,2,3,4]],["decimal numbers",[1.1,1.2,1.3,1.4]],["negative numbers",[-1,-2,-3,-4]]]);
+console.log("Map3");
+console.log(map3);
+
+var map4= new Map([["first name", "lastname"],["Senay","Yakut"]],[["friend1","friend2"],["Neha","Marissa"]]]);
+console.log("Map4");
+console.log(map4);
