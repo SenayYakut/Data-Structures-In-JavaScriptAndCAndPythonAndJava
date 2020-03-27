9e2f7b6d392ddb28791be3ef366114df2edb9348
Senay Yakut <senaykt@gmail.com>
2020-03-27 08:07:25 -0700

    Brute force solution for all positive integer pairs

--- Algorithms/allPositiveIntegerSolutions.js
+++ Algorithms/allPositiveIntegerSolutions.js
@@ -1,1 +1,9 @@
 //Print all positive integer solutions to the equation a^3+b^3=c^3+d^3 where a,b,c,d are integers between 1 and 1000
+// brute force solution but so many memory consumption
+for (i=0;i<;10;i++)
+		for (j=0;j<;10;j++)
+			hashmap[x,a,b] = a^3+b^3;
+	for (i=0;i<;10;i++)
+		for (j=0;j<;10;j++)
+			if c^3+d^3 == hashpmap[x]
+				printf("a, b, c, d");
