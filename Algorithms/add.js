7b2a91d528e1e9a1e3cf78c8bf83258bd9e8dbd7
Senay Yakut <senaykt@gmail.com>
2020-03-30 16:13:19 -0700

    Writing two function which to add parameters 2 or unknown amount

---
+++ Algorithms/add.js
@@ -0,0 +1,22 @@
+/*1- Write a function returns the sum of two numbers
+2- Write a function that returns the sum of all numbers regardless of # of parameters.
+HINT:
+Aritmatic operator
+Rest Operator
+forEach();
+*/
+
+function add1(param1, param2){
+    return param1 + param2;
+  }
+  console.log(add1(2,3));
+  
+  function add2(param){
+    var total=0;
+    param.forEach(num=>{
+      total+=num;
+    });
+    return total;
+  }
+  console.log(add2([1,2,3,4,5]));
+  console.log(add2([30,4,34]));
