715e612c270aaf2b1216ad6b0a61aec07ebd885b
Senay Yakut <senaykt@gmail.com>
2020-04-01 09:06:09 -0700

    BCR is which is O(N) time complexity

---
+++ Algorithms/almostIncreasingSequence.js
@@ -0,0 +1,20 @@
+/*
+Given a sequence of integers as an array, determine wheather it is possibleto obtaina stricklyincreasing sequence by removing no more than one element from the array.
+*/ 
+
+function almostIncreasingSequence(sequence){
+    var nonOrderNum=[];
+
+    for(let i=0; i<sequence.length-1; i++){
+        if(sequence[i]>sequence[i+1]){
+        nonOrderNum.push(sequence[i+1]);
+        }
+    }
+    if(nonOrderNum.length===1) return true;
+    else return false;
+}
+console.log(almostIncreasingSequence([1,3,2]));//true
+console.log(almostIncreasingSequence([1,3,0,4,5,2,]));//false
+console.log(almostIncreasingSequence([1,2,3,4,5]));//false
+
+
