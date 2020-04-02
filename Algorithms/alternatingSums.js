9660cac3bf126b374df74059c4c8b7e159ff3fc5
Senay Yakut <senaykt@gmail.com>
2020-04-02 10:54:58 -0700

    Thet both have Linear time complexity

--- Algorithms/alternatingSums.js
+++ Algorithms/alternatingSums.js
@@ -2,6 +2,7 @@
 Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, and so on.
 You are a given an array of positive integers. the weights of people. Return an array of 2 integers, where the first element is total weights of team 1 and second element is total weight of team 2 after division is complete.
 */
+//Solution 1
 function alternatingSums(arr){
     var weights=[];
     var team1=0;
@@ -10,12 +11,31 @@ function alternatingSums(arr){
     for(var i=0; i<arr.length; i+=2){
         team1 += arr[i];
     }
-    
+
     for(var j=1; j<arr.length; j+=2){
         team2+=arr[j];    
     }
     weights.push(team1,team2);
     return weights;
+}
+console.log(alternatingSums([50,60,60,45,70]));//[180,105]
+console.log(alternatingSums([2,4,1,2,3,5]));//[6,11]
+
+
+//Solution 2
+function alternatingSums(arr){
+    var evenSum=0;
+    var oddSum=0;
+
+    arr.forEach((element, index)=>{
+        if(index%2===0){
+            evenSum+=element;
+        }else{
+            oddSum+=element;
+        }
+    });
+
+    return [evenSum, oddSum];
 }
 console.log(alternatingSums([50,60,60,45,70]));//[180,105]
 console.log(alternatingSums([2,4,1,2,3,5]));//[6,11]
