1d068f71ee36399e3cc3297c027d9a59c225cd40
Senay Yakut <senaykt@gmail.com>
2020-04-02 08:47:48 -0700

    ASCII solution it is O(N) runtime

---
+++ Algorithms/alphabetSubsequence.js
@@ -0,0 +1,24 @@
+/*Check whether the given string is a subsequence of the plaintext alphabet.
+HINT:
+size property
+charCodeAt()
+*/
+
+function alphabetSubsequence(s){
+  const chars=s.split("");
+  const asciiVal=[];
+  
+  chars.forEach(char=>{
+    asciiVal.push(char.charCodeAt(0));
+  });
+  if(new Set(asciiVal).size!==asciiVal.length) return false;
+
+  for(let i=0; i<asciiVal.length; i++){
+      if(asciiVal[i]>asciiVal[i+1]) return false;
+  }
+    return true;
+}
+
+console.log(alphabetSubsequence("effg"));//false
+console.log(alphabetSubsequence("ace"));//true
+console.log(alphabetSubsequence("bxz"));//true
