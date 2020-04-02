0b06cfb569c3f8ed7915d2f05f0fc7f448c688a7
Senay Yakut <senaykt@gmail.com>
2020-04-02 13:23:16 -0700

    O(a+b) runtime which is O(N) Linear

--- Algorithms/areSimilar.js
+++ Algorithms/areSimilar.js
@@ -3,17 +3,19 @@ function areSimilar(a,b){
     return false;
     }
     for(let i=0; i<a.length; i++){
-        if(a[i]!==b[i]){
+        while(a[i]===b[i]){
+            return true;
+        }
             b[i]=b[i+1];
             b[i+1]=b[i];
-            if(a===b) {
+            if(a===b){
                 return true;
             }
-        }
+        
     }
     return false;
 }
 console.log(areSimilar([1,2,3],[1,3,2]));//true
-console.log(areSimilar([1,2,2],[2,1,1]));
+console.log(areSimilar([1,2,2],[2,1,1]));//false
 console.log(areSimilar([1,2,3],[1,3]))//false
-console.log(areSimilar([1,2,3,5,4],[1,3,2,4,5]));//false
+console.log(areSimilar([1,2,3,5,4],[1,3,2,4,5]));//true
