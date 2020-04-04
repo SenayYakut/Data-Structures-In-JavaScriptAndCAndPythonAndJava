eff3270b79d1f2214f78573e5f24916e10a91345
Senay Yakut <senaykt@gmail.com>
2020-04-04 14:26:58 -0700

    Recursion or While loop

---
+++ Algorithms/arrayConversion.js
@@ -0,0 +1,15 @@
+/*
+Given an array of 2k integers(for some integer k),perform the following operations until the array contains only one element
+On the 1st, 3rd, 5th, etc iterations(1-based) replace each pair of consecutive elements with their sum.
+On the 2nd, 4th, 6th etc iterations replace each pair of consecutive elements with their product. After the algorithm has finished, there will be a single element left in the array. Return that element. 
+
+HINT
+push()
+*/
+//Think about recursion or while loop
+function arrayConversion(inputArray){
+    
+       
+}
+
+console.log(arrayConversion([1,2,3,4,5,6,7,8]));
