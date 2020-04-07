b4dde97295a1acb86ba1f626ece485dbe026814d
Senay Yakut <senaykt@gmail.com>
2020-04-07 10:15:39 -0700

    Bishop and Pawn Algorithm

---
+++ Algorithms/bishopAndPawn.js
@@ -0,0 +1,8 @@
+/*
+    Given the position of a white bishop and a black pawn on the standard chess board, determine whether the bishop can capture the pawn in one ove.
+    The bishop has no restrictions in distance for each move, but is limited to diagonal movement. 
+
+    HINT:
+    parseInt()
+*/
+
---
+++ Algorithms/chessBoardCellColor.js
@@ -0,0 +1,21 @@
+/*
+Given two cells on the standard chess board, determine whether they have the same color or not.
+*/
+
+function chessBoardCellColor(cell1, cell2){
+    let board={
+        "a":1,
+        "b":2,
+        "c":3,
+        "d":4,
+        "e":5,
+        "f":6,
+        "g":7,
+        "h":8
+    };
+    let total1=board[cell1[0]]+parseInt(cell1[1]);
+    let total2=board[cell2[0]]+parseInt(cell2[1]);
+
+    return total1%2===total2%2;
+}
+console.log(chessBoardCellColor("a1","c3"));
