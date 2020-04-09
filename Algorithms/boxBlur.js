06c1797e49ad0e8c36ee18f21599a6fdf2072c04
Senay Yakut <senaykt@gmail.com>
2020-04-09 10:46:13 -0700

    Box Blur Algorithm

---
+++ Algorithms/boxBlur.js
@@ -0,0 +1,9 @@
+/*
+Last night you had to study but decided to party instead. Now there is a black and white photo of you that is about to go viral. You cant let this ruin your reputation. so you want to apply box blur algorithm to the photo to hide its content.
+The algorithm works as follows each pixel x in the resulting image has a value equal to the average value of the input image pixels values from 3*3 square with the center at x. All pixels at the edges are cropped.
+As pixels value as an integer, all fractions should be rounded down.
+
+HINTS:
+push()
+Math.floor()
+*/
