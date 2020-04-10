04c726e377bbb041e8f203d70a2bab3edb6a4ca0
Senay Yakut <senaykt@gmail.com>
2020-04-10 10:35:20 -0700

    O(n^4) which is not very optimal

--- Algorithms/boxBlur.js
+++ Algorithms/boxBlur.js
@@ -6,4 +6,28 @@ As pixels value as an integer, all fractions should be rounded down.
 HINTS:
 push()
 Math.floor()
+
+Input: Image:[[36,0,18,9],[27,54,9,0],[81,63,72,45]];
+Output:[[40,30]];
 */
+
+function boxBlur(image){
+    let imageArray=[];
+
+    for(let y=0; y<image.length-2; y++){
+        let line=[];
+        for(let x=0; x<image[y].length-2; x++){
+        let sum=0;
+        for(let a=y; a<y+3; a++){
+            for(let b=x; b<x+3; b++){
+                sum+=image[a][b];
+            }
+          }
+          line.push(Math.floor(sum/9));
+        }
+        imageArray.push(line);
+    }
+    return imageArray;
+} 
+console.log(boxBlur([[36,0,18,9],[27,54,9,0],[81,63,72,45]]));
+
