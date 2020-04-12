ed0b3b40f1470b3a188221d7489002eb7b752f17
Senay Yakut <senaykt@gmail.com>
2020-04-12 10:35:18 -0700

    O(n) linear run time

--- Algorithms/caseInsesitivePalindrome.js
+++ Algorithms/caseInsesitivePalindrome.js
@@ -16,3 +16,14 @@ return reversed===lowerCaseStr;
 }
 console.log(isCaseInsesitivePalindrome("aabaa"));//true
 console.log(isCaseInsesitivePalindrome("Madam"));//true;
+//Solution 2
+function isCaseInsesitivePalindrome(str){
+    var strArr=str.toLowerCase();
+    var newArr=[];
+    for(var i=0; i<strArr.length; i++){
+        newArr.push(strArr[i]);
+    }
+    return newArr.reverse().join("")===strArr;
+}
+console.log(isCaseInsesitivePalindrome("Madam"));//true
+console.log(isCaseInsesitivePalindrome("aabaa"));//true
