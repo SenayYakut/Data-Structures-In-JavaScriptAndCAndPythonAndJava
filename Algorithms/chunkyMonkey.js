3a1b1558ec3df8ba32dba2cc21ddfd626c326599
Senay Yakut <senaykt@gmail.com>
2020-04-13 10:14:04 -0700

    It is O(N) Run Time. I used one pointer to keep track indexes and build a 2 dimensional array

---
+++ Algorithms/chunkyMonkey.js
@@ -0,0 +1,16 @@
+/*
+Write a function that splits an array(first argument)into groups the length of size(second argument)and returns them as a two-dimensional array.
+HINTS:
+slice()
+*/
+
+function chunkyMonkey(arr, size){
+    var nested=[];
+    var counter=0;
+    while(counter<arr.length){
+        nested.push(arr.slice(counter, counter+=size));
+    }
+    return nested;   
+}
+console.log(chunkyMonkey(["a","b","c","d"],2));//[["a","b"],["c","d"]]
+console.log(chunkyMonkey([0,1,2,3,4,5],4));//[[0,1,2,3],[4,5]]
