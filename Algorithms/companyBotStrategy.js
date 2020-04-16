56190d0eabb6053162d41f17666d66e1c627ba05
Senay Yakut <senaykt@gmail.com>
2020-04-16 07:48:41 -0700

    Solution 2 is also O(N) Linear time complexity but cleaner code

--- Algorithms/companyBotStrategy.js
+++ Algorithms/companyBotStrategy.js
@@ -26,3 +26,19 @@ function companyBotStrategy(dataArr){
 console.log(companyBotStrategy([[3,1],[6,1],[4,1],[5,1]]));//4.5
 console.log(companyBotStrategy([[4,-1],[4,-1],[0,0],[6,-1]]));//0
 console.log(companyBotStrategy([[4,1],[4,-1],[0,0],[6,1]]));//5
+
+//Solution 2
+function companyBotStrategy(trainingData){
+    let time=0;
+    let correctness=0;
+    trainingData.forEach(data=>{
+        if(data[1]===1){
+            time+=data[0];
+            correctness+=data[1];
+        }
+    });
+    return time/correctness || 0;
+}
+console.log(companyBotStrategy([[3,1],[6,1],[4,1],[5,1]]));//4.5
+console.log(companyBotStrategy([[4,-1],[4,-1],[0,0],[6,-1]]));//0
+console.log(companyBotStrategy([[4,1],[4,-1],[0,0],[6,1]]));//5
