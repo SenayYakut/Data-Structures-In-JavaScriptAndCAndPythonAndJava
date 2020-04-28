6c8c4ccd279b8c80d6a85cbb711ca8170d44b648
Senay Yakut <senaykt@gmail.com>
2020-04-28 16:14:15 -0700

    Coding Challenges from CodeWars

---
+++ Algorithms/totalAmountOfPoints.js
@@ -0,0 +1,3 @@
+/*
+Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in a collection. Write a function that takes the collection and counts the points our team scored in the championship.
+*/
