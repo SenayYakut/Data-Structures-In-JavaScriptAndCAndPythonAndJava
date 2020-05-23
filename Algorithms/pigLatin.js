b57af2adc6cdfb5cc96384bf55a19d187eee2c50
Senay Yakut <senaykt@gmail.com>
2020-05-22 17:35:37 -0700

    Run time is O(N), Linear. Regular Ex and test method

---
+++ Algorithms/pigLatin.js
@@ -0,0 +1,37 @@
+/*
+Translate the provided string to pig latin.
+Pig latin takes the first consonant(or consonant cluster)of an English word, moves it to the end of word and suffixes an "ay".
+if a word begins with a vowel you just add "way" to the end.
+Input strings are guaranteed to be English words in all lowercase.
+HINTS:
+split()
+test()
+join()
+push()
+*/
+
+function pigLatin(str){
+const letters = str.split("");
+const vowelRegex =  /[aeiou]/;
+
+if(vowelRegex.test(letters[0])){
+    return `${str}way`;
+}
+while(true){
+    if(!vowelRegex.test(letters[0])){
+        letters.push(letters.splice(0,1));
+    }else{
+        break;
+    }
+}
+    str = letters.join("")+"ay";
+    
+    return str;
+}
+console.log(pigLatin("glove"));
+console.log(pigLatin("eight"));
+
+
+
+
+ 
