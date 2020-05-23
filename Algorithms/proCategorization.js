215a0c0c25789f12bc2c16e4b0bd3183edfe0611
Senay Yakut <senaykt@gmail.com>
2020-05-22 19:47:50 -0700

    Thumbtack challenge

--- Algorithms/proCategorization.js
+++ Algorithms/proCategorization.js
@@ -1,3 +1,5 @@
 /*
-Thumbtack helps proffetionals grow their bussiness by identifiying new customers.
+Thumbtack helps Professionals (Pros) grow their business by identifying new customers. Upon registering on Thumbtack, a Pro specifies which categories of services they provide. To help match customer requests with qualified Pros, Thumbtack maintains a list of Pros grouped by service categories.
+
+Given a list of pros and their category preferences, return the list of Pros for each category.
 */
