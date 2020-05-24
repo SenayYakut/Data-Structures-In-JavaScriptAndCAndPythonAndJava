9e502cd47e5aa4a31c3f79a9f0fdb63d5c9a36cb
Senay Yakut <senaykt@gmail.com>
2020-05-24 11:27:51 -0700

    Run Time is O(N), Linear

--- Algorithms/reverseAString.js
+++ Algorithms/reverseAString.js
@@ -3,6 +3,7 @@ Reverse the provided string.
 You may need to turn the string into an array before you can reverse it.
 Your result must be a string.
 */
+//Solution 1
 function reverseAString(str){
 const strChars=str.split("");
 let newStr="";
@@ -10,6 +11,27 @@ for(let i=0; i<str.length; i++){
     newStr+=str[str.length-i-1];
 }
 return newStr;
+}
+console.log(reverseAString("hello"));//olleh
+console.log(reverseAString("Howdy"));//ydwoH
+
+//Solution 2
+function reverseAString(str){
+    const newStr=str.split("").reverse().join("");
+    return newStr;
+
+}
+console.log(reverseAString("hello"));//olleh
+console.log(reverseAString("Howdy"));//ydwoH
+
+//Solution 3
+function reverseAString(str){
+    let newStr="";
+    str=str.split("");
+    for(let i=str.length-1; i>=0; i--){
+        newStr+=str[i];
+    }
+    return newStr;
 }
 console.log(reverseAString("hello"));//olleh
 console.log(reverseAString("Howdy"));//ydwoH
