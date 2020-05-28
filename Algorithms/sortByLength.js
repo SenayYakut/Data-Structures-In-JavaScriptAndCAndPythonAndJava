cbaf6695c7c1fded0eb818ed6c136e1cc3c6e7d0
Senay Yakut <senaykt@gmail.com>
2020-05-28 13:42:07 -0700

    Solution 2 has O(n log n) time complexity and constant O(1) Space complexity

--- Algorithms/sortByLength.js
+++ Algorithms/sortByLength.js
@@ -3,7 +3,7 @@ Given an array of strings, sort them in the order of increasing lengths. If two 
 HINT:
 sort();
 */
-
+//Solution 1
 function sortByLength(arr){
     arr.forEach(elem=>{
         elem.sort((num1, num2)=>{
@@ -15,3 +15,11 @@ function sortByLength(arr){
 console.log(["a","zz","abc","zzz"]);//[ 'a', 'zz', 'abc', 'zzz' ] 
 console.log(["a","zz","zzz","abc"]);//[ 'a', 'zz', 'zzz', 'abc' ] 
 
+//Solution 2
+function sortByLength(arr){
+    return arr.sort(str1, str2=>{
+        return str1.length-str2.length;
+    });
+}
+console.log(["a","zz","abc","zzz"]);//[ 'a', 'zz', 'abc', 'zzz' ] 
+console.log(["a","zz","zzz","abc"]);//[ 'a', 'zz', 'zzz', 'abc' ] 
