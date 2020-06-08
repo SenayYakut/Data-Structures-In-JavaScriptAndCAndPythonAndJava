b62d3bda0cd35e02e4c4b708a6695927df76df2c
Senay Yakut <senaykt@gmail.com>
2020-06-08 11:00:18 -0700

    HashMap takes little extra space but better runtime 0(N+M), IT IS LINEAR MUCH MORE OPTIMAL

--- Algorithms/sumOfTwo.js
+++ Algorithms/sumOfTwo.js
@@ -4,12 +4,11 @@ HINT:
 hasOwnProperty()
 */
 //Solution 1
+
 function sumOfTwo(a,b,v){
-    let sumNumbers=[];
     for(let i = 0; i < a.length; i++){
-        sumNumbers.push(v-a[i]);
     for(let j =0; j<b.length; j++){
-        if(b[j]===sumNumbers[i]){
+        if(b[j]===v-a[i]){
             return true;
          }
        }
@@ -20,18 +19,46 @@ function sumOfTwo(a,b,v){
 console.log(sumOfTwo([1,2,3],[10,20,30,40],42));//true;
 console.log(sumOfTwo([1,2,3],[11,2,300,407],42));//false;
 
-//Solution 2
+
 function sumOfTwo(a,b,v){
     let component=[];
     for(let i = 0; i < a.length; i++){
-        b.forEach(element=>{
-            if(element===(v-a[i])){
-            return true;
-            };
-        });
+       while(!component.includes(v-a[i])){
+           component.push(v-a[i]); 
+       }
     }
-    return false;
+    
+      
+        for(let i =0; i < b.length; i++){
+            if(b.includes(component[i])){
+                return true;
+            }
+        } 
+        return false;  
+
 }
+
+console.log(sumOfTwo([1,2,3,3],[10,20,30,40],42));//true;
 console.log(sumOfTwo([1,2,3],[11,2,300,407],42));//false;
 
 
+//Soution 3 much more optimal
+
+function sumOfTwo(a,b,v){
+    const hashMap = {};
+    for(let num of a){
+        const difference = v-num;
+        hashMap[difference]=difference;
+    }
+    
+    for(let num of b){
+        if(hashMap[num]){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+console.log(sumOfTwo([1,2,3,3],[10,20,30,40],42));//true;
+console.log(sumOfTwo([1,2,3],[11,2,300,407],42));//false;
