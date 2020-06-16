77e24d20422f5a872b759db0a752eaf33c21cea7
Senay Yakut <senaykt@gmail.com>
2020-06-15 18:09:25 -0700

    Permutation Palindrome Algorithm

---
+++ Algorithms/permutationPalindrome.js
@@ -0,0 +1,16 @@
+/*Write an efficient function that checks whether any permutation ↴ of an input string is a palindrome. 
+
+You can assume the input string only contains lowercase letters.
+
+Examples:
+
+"civic" should return True
+"ivicc" should return True
+"civil" should return False
+"livci" should return False
+*/
+
+function permutationPalindrome(str){
+    
+
+}
